27fcfd8837690a211a26403e9a5b58803497dd8a
christianlarry <christianlarry03@gmail.com>
2025-05-24 14:39:45 +0800

    Update filter options to multiple choice

--- src/components/UI/organisms/ProductCatalog.tsx
+++ src/components/UI/organisms/ProductCatalog.tsx
@@ -12,12 +12,19 @@ const ProductCatalog = () => {
   const [paginationPage, setPaginationPage] = useState<number>(1)
 
   // Filter states
-  const [designFilter, setDesignFilter] = useState<SelectOption | null>(null)
-  const [applicationFilter, setApplicationFilter] = useState<SelectOption | null>(null)
-  const [textureFilter, setTextureFilter] = useState<SelectOption | null>(null)
-  const [finishingFilter, setFinishingFilter] = useState<SelectOption | null>(null)
-  const [colorFilter, setColorFilter] = useState<SelectOption | null>(null)
-  const [sizeFilter, setSizeFilter] = useState<SelectOption | null>(null)
+  const [designFilter, setDesignFilter] = useState<SelectOption[] | null>(null)
+  const [applicationFilter, setApplicationFilter] = useState<SelectOption[] | null>(null)
+  const [textureFilter, setTextureFilter] = useState<SelectOption[] | null>(null)
+  const [finishingFilter, setFinishingFilter] = useState<SelectOption[] | null>(null)
+  const [colorFilter, setColorFilter] = useState<SelectOption[] | null>(null)
+  const [sizeFilter, setSizeFilter] = useState<SelectOption[] | null>(null)
+
+  const filterArrToQuery = (key:string, filter:SelectOption[])=>{
+    return filter.map(val=>({
+      key: key,
+      value: val.value
+    }))
+  }
 
   // API call
   const productQuery = httpQuery(
@@ -29,30 +36,12 @@ const ProductCatalog = () => {
         key: "pagination_page",
         value: paginationPage
       },
-      designFilter && {
-        key: "design",
-        value: designFilter.value
-      },
-      applicationFilter && {
-        key: "application",
-        value: applicationFilter.value
-      },
-      textureFilter && {
-        key: "texture",
-        value: textureFilter.value
-      },
-      finishingFilter && {
-        key: "finishing",
-        value: finishingFilter.value
-      },
-      colorFilter && {
-        key: "color",
-        value: colorFilter.value
-      },
-      sizeFilter && {
-        key: "size",
-        value: sizeFilter.value
-      },
+      ...(designFilter ? filterArrToQuery("design", designFilter): []),
+      ...(applicationFilter ? filterArrToQuery("application", applicationFilter): []),
+      ...(textureFilter ? filterArrToQuery("texture", textureFilter): []),
+      ...(finishingFilter ? filterArrToQuery("finishing", finishingFilter): []),
+      ...(colorFilter ? filterArrToQuery("color", colorFilter): []),
+      ...(sizeFilter ? filterArrToQuery("size", sizeFilter): []),
     )
 
   const getProductsResult = getProducts(productQuery,{revalidateOnFocus: false})
@@ -66,7 +55,7 @@ const ProductCatalog = () => {
   //     return
   //   }
 
-  //   const selected = value as SelectOption
+  //   const selected = value as SelectOption[]
 
   //   const selectedSizeArr = selected.value.split("x")
 
@@ -87,49 +76,55 @@ const ProductCatalog = () => {
         <div className="grid gap-4 md:grid-cols-3 lg:grid-cols-6">
           <InputGroup label="Pengaplikasian">
             <Select
+              multiple
               options={getProductFilterOptionsResult.data.data.filter(val => val.type === "application")[0].options || []}
               value={applicationFilter}
-              onChange={(value) => setApplicationFilter(value as SelectOption | null)}
+              onChange={(value) => setApplicationFilter(value as SelectOption[] | null)}
               placeholder="Pilih Lantai/Dinding"
             />
           </InputGroup>
           <InputGroup label="Desain">
             <Select
+              multiple
               options={getProductFilterOptionsResult.data.data.filter(val => val.type === "design")[0].options || []}
               value={designFilter}
-              onChange={(value) => setDesignFilter(value as SelectOption | null)}
+              onChange={(value) => setDesignFilter(value as SelectOption[] | null)}
               placeholder="Pilih Desain"
             />
           </InputGroup>
           <InputGroup label="Tekstur">
             <Select
+              multiple
               options={getProductFilterOptionsResult.data.data.filter(val => val.type === "texture")[0].options || []}
               value={textureFilter}
-              onChange={(value) => setTextureFilter(value as SelectOption | null)}
+              onChange={(value) => setTextureFilter(value as SelectOption[] | null)}
               placeholder="Pilih Tekstur"
             />
           </InputGroup>
           <InputGroup label="Sentuhan Akhir">
             <Select
+              multiple
               options={getProductFilterOptionsResult.data.data.filter(val => val.type === "finishing")[0].options || []}
               value={finishingFilter}
-              onChange={(value) => setFinishingFilter(value as SelectOption | null)}
+              onChange={(value) => setFinishingFilter(value as SelectOption[] | null)}
               placeholder="Pilih Sentuhan Akhir"
             />
           </InputGroup>
           <InputGroup label="Warna">
             <Select
+              multiple
               options={getProductFilterOptionsResult.data.data.filter(val => val.type === "color")[0].options || []}
               value={colorFilter}
-              onChange={(value) => setColorFilter(value as SelectOption | null)}
+              onChange={(value) => setColorFilter(value as SelectOption[] | null)}
               placeholder="Pilih Warna"
             />
           </InputGroup>
           <InputGroup label="Ukuran">
             <Select
+              multiple
               options={getProductFilterOptionsResult.data.data.filter(val => val.type === "size")[0].options || []}
               value={sizeFilter}
-              onChange={(val)=>setSizeFilter(val as SelectOption | null)}
+              onChange={(value)=>setSizeFilter(value as SelectOption[] | null)}
               placeholder="Pilih Ukuran"
             />
           </InputGroup>
