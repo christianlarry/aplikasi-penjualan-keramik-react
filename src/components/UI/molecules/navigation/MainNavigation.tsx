a5129355e08a4454f94057c90fd83fa932de78d7
christianlarry <christianlarry03@gmail.com>
2025-06-04 00:44:37 +0800

    Added dropdown navlink

--- src/components/UI/molecules/navigation/MainNavigation.tsx
+++ src/components/UI/molecules/navigation/MainNavigation.tsx
@@ -1,16 +1,47 @@
 import { Link } from "react-router"
 
 import { MAIN_NAVIGATION } from "../../../../constants/navigation"
+import { LuChevronDown, LuChevronRight } from "react-icons/lu"
 
 const MainNavigation = () => {
   return (
     <nav>
       <ul className="flex gap-4">
-        {MAIN_NAVIGATION.map((val)=>(
-          <li key={val.name}>
-            <Link to={val.path} className="block py-2 hover:underline">
-              {val.name}
+        {MAIN_NAVIGATION.map((firstNavItem)=>(
+          <li key={firstNavItem.name} className="relative group/firstNavItem">
+            <Link key={firstNavItem.name} to={firstNavItem.path} className="block py-2 hover:underline">
+              <div className="flex items-center gap-2">
+                <firstNavItem.Icon className="text-gray-600"/>
+                <p className="font-semibold">{firstNavItem.name}</p>
+              </div>
             </Link>
+
+            {/* DROPDOWN */}
+
+            {firstNavItem.children && 
+            <div className="absolute top-full left-0 z-50 hidden group-hover/firstNavItem:block">
+              <ul className=" bg-white shadow-md rounded-md grid grid-cols-1 lg:grid-cols-2 w-max">
+                {firstNavItem.children.map(secondNavItem=>(
+                  <li>
+                    <Link key={secondNavItem.name} to={firstNavItem.path+secondNavItem.path} className="w-full block p-5 max-w-90 group/secondNavItem">
+                      <div className="flex items-start gap-4">
+                        <div className="p-2 border rounded-md border-gray-300">
+                          <secondNavItem.Icon className="text-gray-500 text-2xl"/>
+                        </div>
+                        <div className="flex flex-col">
+                          <div className="flex items-center gap-1">
+                            <p className="whitespace-nowrap font-semibold group-hover/secondNavItem:underline">{secondNavItem.name}</p>
+                            <LuChevronRight/>
+                          </div>
+                          {secondNavItem.description && <p className="text-gray-500">{secondNavItem.description}</p>}
+                        </div>
+                      </div>
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            </div>
+            }
           </li>
         ))}
       </ul>
--- src/constants/navigation.ts
+++ src/constants/navigation.ts
@@ -1,15 +1,49 @@
-export interface NavigationItem{
-  name:string,
-  path:string
+import type { IconType } from "react-icons"
+import { LuGrid2X2, LuHouse, LuPercent, LuShoppingCart, LuSparkles, LuStar } from "react-icons/lu"
+
+export interface NavigationItem {
+  name: string,
+  description?: string,
+  path: string,
+  Icon: IconType,
+  children?: NavigationItem[]
 }
 
-export const MAIN_NAVIGATION:NavigationItem[] = [
+export const MAIN_NAVIGATION: NavigationItem[] = [
   {
     name: "Beranda",
-    path: "/"
+    path: "/",
+    Icon: LuHouse
   },
   {
     name: "Katalog",
-    path: "/catalog"
+    path: "/catalog",
+    Icon: LuShoppingCart,
+    children: [
+      {
+        name: "Semua Produk",
+        description: "Lihat seluruh koleksi ubin yang tersedia di toko kami.",
+        path: "/all-products",
+        Icon: LuGrid2X2
+      },
+      {
+        name: "Ubin Terlaris",
+        description: "Temukan ubin yang paling banyak dibeli pelanggan.",
+        path: "/best-seller",
+        Icon: LuStar
+      },
+      {
+        name: "Produk Terbaru",
+        description: "Cek ubin terbaru yang baru saja rilis di toko.",
+        path: "/new-arrivals",
+        Icon: LuSparkles
+      },
+      {
+        name: "Sedang Diskon",
+        description: "Lihat ubin-ubin dengan harga promo spesial.",
+        path: "/discount",
+        Icon: LuPercent
+      }
+    ]
   }
 ]
